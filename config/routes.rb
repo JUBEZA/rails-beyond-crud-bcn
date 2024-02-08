7b160ad0f9406fcf9b9316687ea45a0fd8c17807
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-02-08 12:54:47 -0300

    no me encuentra el id :(

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    collection do
-      get :top
-    end
-    member do
-      get :chef
-    end
+    resources :reviews, only: [:new]
   end
+
 end
